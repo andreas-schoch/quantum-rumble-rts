667ed1d8737ad1faf10a25318479b84e96872b32
andreas-schoch <andreas_schoch@outlook.com>
2023-10-24 15:35:00 +0200

    switch to webpack (wasn't able to optimize build size with vite)

---
+++ webpack/webpack.common.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['./src/index.ts'],
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].chunk.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$|\.jsx?$/,
+        include: path.join(__dirname, '../src'),
+        loader: 'ts-loader',
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          filename: '[name].bundle.js',
+        },
+      },
+    },
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({gameName: 'Quantum Rumble', template: 'index.html'})
+  ],
+};
