79eea20a82f32f03e0b5df3a563627545c43c7b2
= <andreas_schoch@outlook.com>
2024-09-14 18:32:14 -0300

    remove rotate feature

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -6,7 +6,6 @@ import { BaseStructure } from '../units/BaseUnit';
 import { Terrain as Terrain } from '../terrain/Terrain';
 import { EmitterManager } from '../Emitter';
 
-type CameraRotations = '0' | '90' | '180' | '270';
 export default class GameScene extends Phaser.Scene {
   observer: Phaser.Events.EventEmitter = new Phaser.Events.EventEmitter();
   controls!: Phaser.Cameras.Controls.SmoothedKeyControl;
@@ -87,15 +86,12 @@ export default class GameScene extends Phaser.Scene {
     camera.centerOnX(GRID * WORLD_X / 2);
     camera.centerOnY(GRID * WORLD_Y / 2);
     // KEYBOARD STUFF
-    let cameraRotationDeg = 0;
-    let rotating = false;
     const keyboard = this.input.keyboard;
     if (!keyboard) throw new Error('cursors is null');
     const keyW = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
     const keyA = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     const keyS = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     const keyD = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-    const keyR = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     const keyONE = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ONE);
     const keyTWO = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.TWO);
     const keyTHREE = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.THREE);
@@ -108,19 +104,12 @@ export default class GameScene extends Phaser.Scene {
     const keyESC = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
     const keyX = keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.X);
 
-    const rotKeyMap: {[key in CameraRotations]: [Phaser.Input.Keyboard.Key, Phaser.Input.Keyboard.Key, Phaser.Input.Keyboard.Key, Phaser.Input.Keyboard.Key]} = {
-      '0': [keyW, keyA, keyS, keyD],
-      '90': [keyD, keyW, keyA, keyS],
-      '180': [keyS, keyD, keyW, keyA],
-      '270': [keyA, keyS, keyD, keyW],
-    };
-
     this.controls = new Phaser.Cameras.Controls.SmoothedKeyControl({
       camera,
-      up: rotKeyMap[String(cameraRotationDeg) as CameraRotations][0],
-      left: rotKeyMap[String(cameraRotationDeg) as CameraRotations][1],
-      down: rotKeyMap[String(cameraRotationDeg) as CameraRotations][2],
-      right: rotKeyMap[String(cameraRotationDeg) as CameraRotations][3],
+      up: keyW,
+      left: keyA,
+      down: keyS,
+      right: keyD,
       zoomIn: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q),
       zoomOut: keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
       acceleration: 10,
@@ -130,20 +119,6 @@ export default class GameScene extends Phaser.Scene {
       minZoom: MIN_ZOOM,
       zoomSpeed: 0.05,
     });
-
-    keyR.onDown = evt => {
-      if (rotating) return;
-      if(evt.ctrlKey) return;
-      rotating = true;
-      cameraRotationDeg = cameraRotationDeg + 90;
-      if (cameraRotationDeg >= 360) cameraRotationDeg = 0;
-      camera.rotateTo(Phaser.Math.DegToRad(cameraRotationDeg), false, 350, 'Linear', false, (ref, progress) => {if (progress === 1) rotating = false;});
-      if (!this.controls) return;
-      this.controls.up = rotKeyMap[String(cameraRotationDeg) as CameraRotations][0];
-      this.controls.left = rotKeyMap[String(cameraRotationDeg) as CameraRotations][1];
-      this.controls.down = rotKeyMap[String(cameraRotationDeg) as CameraRotations][2];
-      this.controls.right = rotKeyMap[String(cameraRotationDeg) as CameraRotations][3];
-    };
 
     // TODO deduplicate
     keyONE.onDown = () => this.selectUnit(0);
